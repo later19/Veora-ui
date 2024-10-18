9bab7504bebd3f93dfd49dc5c5e864639b19cd64
later19 <1764067223@qq.com>
2024-10-18 13:51:15 +0800

    init

---
+++ play/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <!-- If you want to apply a custom namespace, uncomment the following and apply the css configuration in `vite.config.ts`. -->
+  <!-- <el-config-provider namespace="ep"> -->
+  <div class="play-container">
+    test
+    <!-- <el-icon color="#409EFC">
+      <el-icon-check />
+    </el-icon>
+    <el-icon>
+      <el-icon-menu />
+    </el-icon>
+    <el-link type="success">test</el-link>
+    <div v-loading="true" /> -->
+  </div>
+  <!-- </el-config-provider> -->
+</template>
+
+<script setup lang="ts">
+// code here
+</script>
+
+<style>
+html,
+body {
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+}
+
+#play {
+  height: 100%;
+  width: 100%;
+  text-align: center;
+}
+</style>

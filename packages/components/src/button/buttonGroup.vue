d870ef7ca6ddf77e8383517c80a82ac98612b23f
later19 <1764067223@qq.com>
2024-10-30 16:18:02 +0800

    feat:Warning handling

---
+++ packages/components/src/button/buttonGroup.vue
@@ -0,0 +1,62 @@
+<template>
+  <button
+    class="button"
+    disabled
+    :class="[
+      typeClass,
+      sizeClass,
+      { 'is-plain': plain },
+      { 'is-round': round },
+      { 'is-circle': circle },
+      { 'is-disabled': disabled },
+      { 'is-link': link },
+    ]"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script name="VButton" lang="ts" setup>
+import { computed } from 'vue'
+const props = defineProps({
+  type: {
+    type: String,
+    default: 'default',
+    validator: (value: string) =>
+      ['default', 'success', 'primary', 'error', 'warning', 'info'].includes(
+        value,
+      ),
+  },
+  size: {
+    type: String,
+    default: 'default',
+    validator: (value: string) => ['default', 'large', 'small'].includes(value),
+  },
+  plain: {
+    type: Boolean,
+    default: false,
+  },
+  round: {
+    type: Boolean,
+    default: false,
+  },
+  circle: {
+    type: Boolean,
+    default: false,
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  link: {
+    type: Boolean,
+    default: false,
+  },
+})
+const typeClass = computed(() => `button-${props.type}`)
+const sizeClass = computed(() => `button-${props.size}`)
+</script>
+
+<style lang="scss" scoped>
+@import url(./style/index.scss);
+</style>
--- packages/components/src/button/index.ts
+++ packages/components/src/button/index.ts
@@ -1,5 +1,8 @@
 import { withInstall } from '../../../utils/index'
 import button from './index.vue'
+import buttonGroup from './buttonGroup.vue'
 
 const Button = withInstall(button)
-export default Button
+const ButtonGroup = withInstall(buttonGroup)
+
+export default { Button, ButtonGroup }
